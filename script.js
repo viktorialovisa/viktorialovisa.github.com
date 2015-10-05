b1eec433784b414437b6ceb6b34399edfcab3a66
viktorialovisa <viktorialovisa@gmail.com>
2015-10-05 17:05:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("foot01").innerHTML =
 "<p>&copy;  " + new Date().getFullYear() + " W3Schools. All rights reserved.</p>";
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
-"<li><a href='index.html'>Home</a></li>" +
-"<li><a href='customers.html'>Data</a></li>" +
-"<li><a href='about.html'>About</a></li>" +
+"<li><a href='index.html'>Heim</a></li>" +
+"<li><a href='customers.html'>Vörumerki</a></li>" +
+"<li><a href='about.html'>Um Icelandic Deisgn</a></li>" +
 "</ul>";
